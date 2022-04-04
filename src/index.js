34641a4967cdbb7b16d81d44cb6a7466d296fcb3
Benjamin <shallifi@hotmail.com>
2022-04-04 17:15:06 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById("create-task-form")
+  createTaskForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+  })
+  createTaskForm.addEventListener("submit", newTask);
+  const createNewTask = document.getElementById("new-task-description")
+  
+  // document.querySelector('form').addEventListener('submit', (e) => {
+
+  
+
+  // handleTD(e.target.MyTodos.value)
+  
+  
+  function newTask(e) {
+    e.preventDefault();
+    // console.log(e.target)
+    handleTD(createNewTask.value)
+    createTaskForm.reset()
+  }
+ 
 });
+  // your code here
+
+function handleTD(todo) {
+  let p = document.createElement('p')
+  p.textContent = todo
+  console.log(p)
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+
+}
